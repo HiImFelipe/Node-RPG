4101db6fce465f95da3ff4c83840788dcbbaf170
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-01 15:42:35 -0300

    hotfix: Remove dist folder from git repository

--- dist/constants/colors.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    primary: string;
-    secondary: string;
-    neutral: string;
-    contrast: string;
-    constrastSecondary: string;
-};
-export default _default;
--- dist/constants/colors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    primary: "#AD558A",
-    secondary: "#7A1753",
-    neutral: "#C284E0",
-    contrast: "#E4E3C0",
-    constrastSecondary: "#ADA255",
-};
