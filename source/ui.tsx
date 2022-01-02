5bad02430c422ff7b307a75c66275695aee47471
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-02 12:18:31 -0300

    fix: Make ui.tsx render our scene loader

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,49 +1,13 @@
 import React, { FC } from "react";
-import { Box, Text } from "ink";
-import SelectInput from "ink-select-input";
 
-import colors from "./constants/colors";
-
-interface IOption {
-	label: string;
-	value: string;
-}
-
-const options: IOption[] = [
-	{
-		label: "New Game",
-		value: "newgame",
-	},
-	{
-		label: "Quit",
-		value: "quit",
-	},
-];
-
-const handleSelect = (item: IOption) => {
-	switch (item.value) {
-		case "quit":
-			process.exit(0);
-		default:
-			return;
-	}
-};
+import UserProvider from "./contexts/userContext";
+import SceneLoader from "./scenes/SceneLoader";
 
 const App: FC = () => {
 	return (
-		<Box flexDirection="column" borderStyle="double" padding={1}>
-			<Box marginBottom={2} marginLeft={2}>
-				<Text>
-					Welcome to <Text color={colors.primary}>Node RPG</Text>!
-				</Text>
-			</Box>
-
-			<SelectInput
-				items={options}
-				color={colors.contrast}
-				onSelect={handleSelect}
-			/>
-		</Box>
+		<UserProvider>
+			<SceneLoader />
+		</UserProvider>
 	);
 };
 
