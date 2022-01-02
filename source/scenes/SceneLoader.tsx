e1a4ec2ac0b84a0eadbe7c427830888178c08a19
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-02 12:20:05 -0300

    feat: Create our scene loader

---
+++ source/scenes/SceneLoader.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useEffect, useState } from "react";
+
+//import { useUser } from "../contexts/userContext";
+import Menu from "./menu";
+
+const SceneLoader: FC = () => {
+	//const { userData } = useUser();
+	const userData = { currentMap: "training field" };
+	const [componentToRender, SetComponentToRender] = useState(() => Menu);
+
+	useEffect(() => {
+		const importDynamicComponent = async () => {
+			if (userData && userData.currentMap) {
+				const {default: Component} = await import(`./${userData.currentMap}`);
+				return SetComponentToRender(() => Component);
+			}
+
+			return null;
+		};
+
+		importDynamicComponent();
+	}, []);
+
+	return React.createElement(componentToRender);
+};
+
+export default SceneLoader;
