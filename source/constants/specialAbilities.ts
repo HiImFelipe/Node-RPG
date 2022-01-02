f36f59a355a18db6b39b943379eecd95feea0e0d
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-02 14:56:56 -0300

    fix: Refactor special abilities file

--- source/constants/specialAbilities.ts
+++ source/constants/specialAbilities.ts
@@ -1,4 +1,4 @@
-export const archerAbilities = [
+export const ARCHER_ABILITIES = [
     { id: 1, name: "Fire Arrow", attack: 52, manaCost: 20 },
     { id: 2, name: "Rapid Throw", attack: 13, manaCost: 5 },
     { id: 3, name: "Ice Cold", attack: 5, manaCost: 40 },
