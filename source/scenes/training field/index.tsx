3e46ae9e0ff82109537444c77483ec8adc743a47
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-02 12:20:25 -0300

    feat: Create training field scene

---
+++ source/scenes/training field/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "ink";
+
+const TrainingField = () => {
+	return (
+		<Box>
+			<Text>Salve</Text>
+		</Box>
+	);
+};
+
+export default TrainingField;
