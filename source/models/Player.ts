a3ff379225e83a5702da1c868c5a1759a5c3111f
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-02 14:42:21 -0300

    feat: Create Player abstract entity

---
+++ source/models/Player.ts
@@ -0,0 +1,12 @@
+export abstract class Player implements IPlayer {
+	public name = "";
+	public specialAbilities: ISpecialAbility[] = [];
+
+	constructor(public speed = 0, public attack = 0, public defense = 0) {}
+
+	setName(name: string) {
+		this.name = name;
+	}
+
+	abstract addSpecialAbility(id: number): void;
+}
